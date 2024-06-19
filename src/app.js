e2b3fa7536000512990ca08f73c722b8c6d5a1ec
Tatang Romadhona <romadhonatatang@gmail.com>
2024-06-19 08:13:01 +0700

    add api docs

--- src/app.js
+++ src/app.js
@@ -1,35 +1,48 @@
-// api
+require('dotenv').config();
+const { PORT } = process.env;
 const cors = require('cors');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs");
+const YAML = require('yaml');
+const cron = require('node-cron');
+const moment = require('moment-timezone');
+const random = require('random-name');
+const generateFlights = require('./tasks/generatefligts');
+const router = require('./routers/flight');
+
+// Import dependencies
+
+// Read Swagger file
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
 const app = express();
 
 app.use(cors());
-const router = require('./routers/flight');
 app.use('/flights', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// default route
 let name = 'World';
 app.get('/', (req, res) => {
     res.send('Hello ' + name + '!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Start server
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
 });
 
-// cron
-const cron = require('node-cron');
-const moment = require('moment-timezone');
-const random = require('random-name');
-const generateFlights = require('./tasks/generatefligts');
-
-// generate flights every day at 00:00
+// Schedule cron jobs
+// Generate flights every day at 00:00
 cron.schedule('0 0 * * *', () => {
     let date = moment().add(30, 'days').format('YYYY-MM-DD');
     generateFlights(date);
     console.log('Running generateFlights cron job for date:', date);
 });
 
-// test cron job every minute
+// Test cron job every minute
 cron.schedule('* * * * *', () => {
     name = random.first();
 });
